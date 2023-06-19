8d1b5008c604816cfc421a30279676bf17ef201c
Haqq Bassit <munagahmnh@gmail.com>
2023-06-19 11:23:11 +0000

    add max sum of hourglass

--- max_sum_of_hourglass.py
+++ max_sum_of_hourglass.py
@@ -1,8 +1,9 @@
-grid = [[6,2,1,3],[4,2,1,5],[9,2,8,7],[4,1,2,9]]
-maximum = 0
-for i in range(len(grid[0])-2):
-    sum_hourglass = 0
-    for j in range(len(grid)-3):
-        sum_hourglass = sum(grid[i][j:j+3]) + grid[i+1][j+1] + sum(grid[i+2][j:j+3])
-        maximum = max(sum_hourglass, maximum)
-print(maximum)
+class Solution:
+    def maxSum(self, grid: List[List[int]]) -> int:
+        maximum = 0
+        for i in range(len(grid)-2):
+            sum_hourglass = 0
+            for j in range(len(grid[0])-2):
+                sum_hourglass = sum(grid[i][j:j+3])+grid[i+1][j+1]+sum(grid[i+2][j:j+3])
+                maximum = max(sum_hourglass, maximum)
+        return maximum
